27587633d6a71f930fcc827fbda2e7ccb8e098e6
melissabutler <melissaebutler2015@gmail.com>
2023-09-20 17:57:07 -0400

    CODE IT WORKING

--- app.js
+++ app.js
@@ -12,47 +12,105 @@ const savedList = JSON.parse(localStorage.getItem('todos')) || [];
 for(let i = 0; i < savedList.length; i++){
   let newTask = document.createElement('li');
   newTask.innerHTML = savedList[i].item;
+  newTask.isCompleted = savedList[i].isCompleted ? true : false;
+  if(newTask.isCompleted) {
+    newTask.style.textDecoration = 'line-through';
+  }
   toDoList.appendChild(newTask);
-  // newTask.isCompleted = savedList[i].isCompleted ? true : false;
-  // if(newTask.isCompleted) {
-  //   newLi.style.textDecoration = "line-through";
+  newTask.isCompleted = savedList[i].isCompleted ? true : false;
+  if(newTask.isCompleted) {
+    newTask.style.textDecoration = "line-through";
   };
-
+};
 //Create new todo when submitted
 newTodoForm.addEventListener("submit", function(event) {
   event.preventDefault();
-  let newLi = document.createElement('li');
+  const newLi = document.createElement('li');
   let newToDoInput = document.querySelector('#item').value;
   newLi.innerText = newToDoInput;
   newLi.isCompleted = false;
 
+  // let completeButton = document.createElement('button');
+  // completeButton.innerText = "Completed";
+  // newLi.append(completeButton);
+
+  let removeButton = document.createElement('button');
+  // removeButton.className = removeBtn;
+  removeButton.innerText = "Remove";
+  newLi.append(removeButton) 
+
+
   newTodoForm.reset();
   toDoList.append(newLi);
-  //change state of completion
-  let completeButton = document.createElement('button');
-  completeButton.innerText = "Completed";
-  completeButton.addEventListener('click', function(e){
-    if((newLi.style.textDecoration == "line-through") === false){
-      newLi.style.textDecoration = "line-through";
-      newLi.isCompleted = true;
-    } else {
-      newLi.style.textDecoration = "";
-      newLi.isCompleted = false;
-        }
-      });
-  newLi.append(completeButton);
 
-  // remove list item
-  let removeButton = document.createElement('button');
-  removeButton.innerText = "Remove";
-  removeButton.addEventListener('click', function(e) {
-      e.target.parentElement.remove();
-    })
-  newLi.append(removeButton)
-   //save to localStorage
-  savedList.push({ item: newLi.innerHTML, isCompleted: false});
+  savedList.push({ item: newLi.innerHTML, isCompleted: false,});
+    localStorage.setItem('todos', JSON.stringify(savedList));
+});
+
+function findIndex(target){
+  for(let i = 0; i< savedList.length; i++){
+    if(savedList[i].item === target.innerHTML) {
+      let foundIndex = [i];
+      return(foundIndex);
+    }
+  }
+}
+
+toDoList.addEventListener('click', function(e) {
+  e.preventDefault();
+  //if clicked item is a button, do remove
+  let target = e.target;
+  let removeMe = e.target.parentElement;
+  let itemTag = e.target.tagName;
+
+  //remove item
+  if(itemTag.toLowerCase() === 'button'){
+    removeMe.remove();
+    let storageIndex = findIndex(removeMe);
+    savedList.splice(storageIndex, 1);
+    localStorage.setItem('todos', JSON.stringify(savedList));
+};
+  //update item between completed not
+   if(itemTag.toLowerCase() === 'li') {
+    if(!target.isCompleted) {
+      let storageIndex = findIndex(target);
+      target.isCompleted = true;
+      target.style.textDecoration = 'line-through';
+      savedList[storageIndex].isCompleted = true;
+    } else if(target.isCompleted) {
+      let storageIndex = findIndex(target);
+      target.isCompleted = false;
+      target.style.textDecoration = "none";
+      savedList[storageIndex].isCompleted = false;
+    }
+
   localStorage.setItem('todos', JSON.stringify(savedList));
-  });
+}
+
+});
+  //if clicked item is li item, strikethrough
+ 
+
+
+
+  // toDoList.addEventListener('click', function(e){
+  //   if((newLi.style.textDecoration == "line-through") === false){
+  //     newLi.style.textDecoration = "line-through";
+  //     newLi.isCompleted = true;
+  //   } else {
+  //     newLi.style.textDecoration = "";
+  //     newLi.isCompleted = false;
+  //       }
+  //     });
+ 
+
+  
+
+    
+   //update localStorage
+
+  
+  ;
   
 ;
 
